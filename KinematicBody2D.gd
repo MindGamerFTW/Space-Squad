f38c1fa06586e3453ed468c844d5550438ac8af6
Maxim <maximxi321@gmail.com>
2018-08-23 22:09:45 +0300

    Added a shit load of stuff

---
+++ KinematicBody2D.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		$Sprite.play("right")
+		motion.x = 200
+	elif Input.is_action_pressed("ui_left"):
+		$Sprite.play("left")
+		motion.x = -200
+	elif Input.is_action_pressed("ui_up"):
+		$Sprite.play("back")
+		motion.y = -200
+	elif Input.is_action_pressed("ui_down"):
+		$Sprite.play("front")
+		motion.y = 200
+	else:
+		motion.x = 0
+		motion.y = 0
+	
+	move_and_slide(motion)
+	pass
