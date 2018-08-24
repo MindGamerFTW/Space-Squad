c615b7d3f804973cb80788295fcb6c848f411938
omer <omer9955821@gmail.com>
2018-08-24 13:57:43 +0300

    added wall

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -7,18 +7,26 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		$Sprite.play("right")
 		motion.x = 200
+		motion.y = 0
+		
 	elif Input.is_action_pressed("ui_left"):
 		$Sprite.play("left")
 		motion.x = -200
+		motion.y = 0
+		
 	elif Input.is_action_pressed("ui_up"):
 		$Sprite.play("back")
 		motion.y = -200
+		motion.x = 0
+		
 	elif Input.is_action_pressed("ui_down"):
 		$Sprite.play("front")
 		motion.y = 200
+		motion.x = 0
+		
 	else:
 		motion.x = 0
 		motion.y = 0
 	
 	move_and_slide(motion)
-	pass
+	
